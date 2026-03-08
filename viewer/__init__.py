b6b675bf713c74828764887aeedd8cf6b66b1ee8
Carter Francis <cartsfrancis@gmail.com>
2026-03-07 19:30:55 -0600

    New Feature: Add __init__.py to expose viewer and figure components

---
+++ viewer/__init__.py
@@ -0,0 +1,6 @@
+from viewer.viewer1d import Viewer1D
+from viewer.viewer2d import Viewer2D
+from viewer.figure import Figure, GridSpec, SubplotSpec, subplots
+
+__all__ = ["Viewer1D", "Viewer2D", "Figure", "GridSpec", "SubplotSpec", "subplots"]
+
